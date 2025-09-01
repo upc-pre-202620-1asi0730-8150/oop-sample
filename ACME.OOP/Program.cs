a3b9aac01803abff56a995f5f3e3bae12a2bb4c0
Angel Velasquez <pcisavel@upc.edu.pe>
2025-08-31 21:14:22 -0500

    feat(main): add main behavior.

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ACME.OOP.Procurement.Domain.Model.Aggregates;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.Aggregates;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
 
-Console.WriteLine("Hello, World!");
+var supplierAddress = new Address("Supplier St", "123", "SupplierCity", null, "12345", "United States");
+var supplier = new Supplier("SUP001", "Microsoft, Inc.", supplierAddress);
+
+var purchaseOrder = new PurchaseOrder("PO001", new SupplierId(supplier.Identifier), DateTime.Now, "USD");
+purchaseOrder.AddItem(ProductId.New(), 10, 25.99m);
+purchaseOrder.AddItem(ProductId.New(), 20, 19.99m);
+
+Console.WriteLine($"Purchase Order {purchaseOrder.OrderNumber} created for Supplier ID {purchaseOrder.SupplierId.Identifier} in {purchaseOrder.Currency}");
+foreach (var item in purchaseOrder.Items)
+{
+    Console.WriteLine($"Order Item Total: {item.CalculateItemTotal()}");
+}
+Console.WriteLine($"Order Total: {purchaseOrder.CalculateTotal()}");
