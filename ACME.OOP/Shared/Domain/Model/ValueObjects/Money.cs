ccabad235a6a8f5796509b0b5b25c5c6303b0162
Angel Velasquez <pcisavel@upc.edu.pe>
2026-03-31 00:00:32 -0500

    feat(money): add add and multiply methods for monetary operations.

--- ACME.OOP/Shared/Domain/Model/ValueObjects/Money.cs
+++ ACME.OOP/Shared/Domain/Model/ValueObjects/Money.cs
@@ -26,4 +26,21 @@ public record Money
     /// </summary>
     /// <returns>A string in the format "Amount Currency".</returns>
     public override string ToString() => $"{Amount} {Currency}";
+
+    /// <summary>
+    /// Adds two <see cref="Money"/> objects together.
+    /// </summary>
+    /// <param name="other">The other <see cref="Money"/> object to add. If null, the original <see cref="Money"/> object is returned.</param>
+    /// <returns>A new <see cref="Money"/> object representing the sum of the two monetary values.</returns>
+    public Money Add(Money? other)
+    {
+        return other == null ? this : new Money(Amount + other.Amount, Currency);
+    }
+    
+    /// <summary>
+    /// Multiplies the monetary value by a factor.
+    /// </summary>
+    /// <param name="factor">The factor to multiply the monetary value by.</param>
+    /// <returns>A new <see cref="Money"/> object representing the result of the multiplication.</returns>
+    public Money Multiply(int factor) => new(Amount * factor, Currency);
 }
