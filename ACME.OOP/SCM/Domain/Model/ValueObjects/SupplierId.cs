faf4ff0048542015e2f917a3e33a6a04a6d24d1b
Angel Velasquez <pcisavel@upc.edu.pe>
2026-08-18 16:13:08 -0500

    refactor(domain): add field-based validation for SupplierId, Supplier properties

--- ACME.OOP/SCM/Domain/Model/ValueObjects/SupplierId.cs
+++ ACME.OOP/SCM/Domain/Model/ValueObjects/SupplierId.cs
@@ -5,18 +5,22 @@ namespace ACME.OOP.SCM.Domain.Model.ValueObjects;
 /// </summary>
 public readonly record struct SupplierId
 {
-    public string Identifier { get; init; }
+    public string Identifier
+    {
+        get;
+        init
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+            field = value;
+        }
+    }
 
     /// <summary>
     /// Creates a new instance of <see cref="SupplierId"/>. 
     /// </summary>
     /// <param name="identifier">The unique identifier for the supplier.</param>
     /// <exception cref="ArgumentException">Thrown when the identifier is null or empty.</exception>
-    public SupplierId(string identifier)
-    {
-        ArgumentException.ThrowIfNullOrWhiteSpace(identifier);
-        Identifier = identifier;
-    }
+    public SupplierId(string identifier) => Identifier = identifier;
 
     public override string ToString() => Identifier;
 }
